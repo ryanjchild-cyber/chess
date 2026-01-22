1845995356f438737ae082f84f4afb021e91e943
ryanjchild-cyber <ryanjchild@gmail.com>
2026-01-21 19:54:09 -0700

    Implemented Phase 0 basic move functionality. Added equals and hashCode so the tests would actually be correct.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,8 +12,12 @@ import java.util.List;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor=pieceColor;
+        this.type=type;
     }
 
     /**
@@ -30,14 +36,28 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -48,6 +68,35 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        Collection<ChessMove> moves=new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+
+        if (piece.getPieceType()==PieceType.BISHOP) {
+            int[][] directions={
+                    {1,1},
+                    {1,-1},
+                    {-1,1},
+                    {-1,-1}
+            };
+            for (int[] movement: directions) {
+                int row=myPosition.getRow()+movement[0];
+                int col= myPosition.getColumn()+movement[1];
+                while (row>=1 && row<=8 && col>=1 && col<=8) {
+                    ChessPosition newPosition=new ChessPosition(row,col);
+                    ChessPiece target=board.getPiece(newPosition);
+                    if (target == null) {
+                        moves.add(new ChessMove(myPosition,newPosition,null));
+                    } else {
+                        if (target.getTeamColor() != piece.getTeamColor()) {
+                            moves.add(new ChessMove(myPosition,newPosition, null));
+                        }
+                        break;
+                    }
+                    row+=movement[0];
+                    col+=movement[1];
+                }
+            }
+        }
+        return moves;
     }
 }
