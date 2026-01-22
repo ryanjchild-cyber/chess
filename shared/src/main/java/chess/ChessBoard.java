64d4e477a7d58453c2ac6ad86c85271709cd68f8
ryanjchild-cyber <ryanjchild@gmail.com>
2026-01-21 22:17:58 -0700

    Implemented resetBoard, and added more equals and hashCode functions.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -34,11 +37,46 @@ public class ChessBoard {
         return squares[position.getRow()-1][position.getColumn()-1];
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (int col = 1; col <= 8; col++) {
+            addPiece(new ChessPosition(2,col),
+                    new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7,col),
+                    new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+            ChessPiece.PieceType[] backRank = {
+                    ChessPiece.PieceType.ROOK,
+                    ChessPiece.PieceType.KNIGHT,
+                    ChessPiece.PieceType.BISHOP,
+                    ChessPiece.PieceType.QUEEN,
+                    ChessPiece.PieceType.KING,
+                    ChessPiece.PieceType.BISHOP,
+                    ChessPiece.PieceType.KNIGHT,
+                    ChessPiece.PieceType.ROOK
+            };
+            for (int column = 1; column <= 8; column++) {
+                addPiece(new ChessPosition(1, column),
+                        new ChessPiece(ChessGame.TeamColor.WHITE, backRank[column - 1]));
+                addPiece(new ChessPosition(8, column),
+                        new ChessPiece(ChessGame.TeamColor.BLACK, backRank[column - 1]));
+            }
+        }
     }
 }
