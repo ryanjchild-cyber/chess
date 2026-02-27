32dd3c32166d99d3fa2919fcf9d3e57fd9630ada
ryanjchild-cyber <ryanjchild@gmail.com>
2026-02-26 17:44:55 -0700

    Added constants to Server.java but will switch to implementing Service first.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,10 +1,17 @@
 package server;
-
+import com.google.gson.Gson;
+import dataaccess.*;
 import io.javalin.*;
+import service.*;
 
 public class Server {
 
     private final Javalin javalin;
+    private final Gson gson=new Gson();
+    private final DataAccess dao=new MemoryDataAccess();
+    private final ClearService clearService=new ClearService(dao);
+    private final UserService userService=new UserService(dao);
+    private final GameService gameService=new GameService(dao);
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
