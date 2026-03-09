9a4b34d0b25b229f876eaf59edd6758d4d7529b9
ryanjchild-cyber <ryanjchild@gmail.com>
2026-03-08 22:30:47 -0600

    Added db.properties file and began DatabaseManager.java

---
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -0,0 +1,8 @@
+package dataaccess;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+public class DatabaseManager {
+}
