fb4270e57441a821e31f071c896a01e2b3220238
ryanjchild-cyber <ryanjchild@gmail.com>
2026-03-09 00:20:31 -0600

    I forgot to read that there was starter code, so I replaced the code I wrote. I also started MySQLDataAccess.java.

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -1,38 +1,77 @@
 package dataaccess;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.Properties;
+
 public class DatabaseManager {
-    private static final Properties properties=new Properties();
+    private static String databaseName;
+    private static String dbUsername;
+    private static String dbPassword;
+    private static String connectionUrl;
+
+    /*
+     * Load the database information for the db.properties file.
+     */
     static {
-        try (InputStream input=DatabaseManager.class.getClassLoader().getResourceAsStream("db.properties")) {
-            if (input==null) {
-                throw new RuntimeException("db.properties not found");
+        loadPropertiesFromResources();
+    }
+
+    /**
+     * Creates the database if it does not already exist.
+     */
+    static public void createDatabase() throws DataAccessException {
+        var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create database", ex);
+        }
+    }
+
+    /**
+     * Create a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DatabaseManager.getConnection()) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    static Connection getConnection() throws DataAccessException {
+        try {
+            //do not wrap the following line with a try-with-resources
+            var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get connection", ex);
+        }
+    }
+
+    private static void loadPropertiesFromResources() {
+        try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+            if (propStream == null) {
+                throw new Exception("Unable to load db.properties");
             }
-            properties.load(input);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load db.properties",e);
+            Properties props = new Properties();
+            props.load(propStream);
+            loadProperties(props);
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties", ex);
         }
     }
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                properties.getProperty("db.url"),
-                properties.getProperty("db.user"),
-                properties.getProperty("db.password")
-        );
-    }
-    public static void createDatabase() throws SQLException {
-        String url=properties.getProperty("db.url");
-        String user=properties.getProperty("db.user");
-        String password=properties.getProperty("db.password");
-        int lastSlash=url.lastIndexOf("/");
-        String serverUrl=url.substring(0,lastSlash);
-        String dbName=url.substring(lastSlash+1);
-        try (Connection connection=DriverManager.getConnection(serverUrl,user,password);
-            var statement=connection.createStatement()) {
-            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS "+dbName);
-        }
+
+    private static void loadProperties(Properties props) {
+        databaseName = props.getProperty("db.name");
+        dbUsername = props.getProperty("db.user");
+        dbPassword = props.getProperty("db.password");
+
+        var host = props.getProperty("db.host");
+        var port = Integer.parseInt(props.getProperty("db.port"));
+        connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
     }
 }
---
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -0,0 +1,16 @@
+package dataaccess;
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+public class MySQLDataAccess implements DataAccess {
+
+}
+
