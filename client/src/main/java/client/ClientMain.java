2d8e3a9d676ae286ccfc20a07fd644f2ca2061c8
ryanjchild-cyber <ryanjchild@gmail.com>
2026-03-23 13:17:30 -0600

    Finished first iteration of ClientMain.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,21 @@
 package client;
-
+import ui.Repl;
 import chess.*;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        //System.out.println("♕ 240 Chess Client: " + piece);
+        int port=8080;
+        if (args.length>0) {
+            try {
+                port=Integer.parseInt(args[0]);
+            } catch (NumberFormatException ignored) {
+                System.out.println("Using default port 8080");
+            }
+        }
+        var serverFacade=new ServerFacade(port);
+        var repl=new Repl(serverFacade);
+        repl.run();
     }
 }
