93c11551d9aaf3518bb0fa3d55b403ac68c3355a
ryanjchild-cyber <ryanjchild@gmail.com>
2026-04-07 13:51:25 -0600

    Finished commands and moving on to messages.

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,25 @@
+package websocket.commands;
+import chess.ChessMove;
+import java.util.Objects;
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+    public ChessMove getMove() {
+        return move;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) {
+            return false;
+        }
+        MakeMoveCommand that = (MakeMoveCommand) o;
+        return Objects.equals(move, that.move);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), move);
+    }
+}
